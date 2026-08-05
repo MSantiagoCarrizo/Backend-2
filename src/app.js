a3e146e66e83a6e646644b85514780b0ca77233e
Santi Carrizo <carrizo7802santi@gmail.com>
2026-08-05 18:15:15 -0300

    Dependencias para autenticacion JWT

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", routes);
 
 app.use(errorHandler);
 
 export default app;
+

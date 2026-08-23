b9e1b70b90f5adccc388f8d238b26ca4b2791e6c
Santi Carrizo <carrizo7802santi@gmail.com>
2026-08-22 22:53:49 -0300

    Creación de eventos protegida por rol

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,6 +1,20 @@
-export const getEvents = async (req, res) => {
+import eventsService from "../services/events.service.js";
+
+export const getEvents = async (req, res, next) => {
     try {
-        res.status(200).json({ status: "success", payload: [] });
+        const events = await eventsService.getEvents();
+
+        return res.status(200).json({ status: "success", payload: events });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const createEvent = async (req, res, next) => {
+    try {
+        const newEvent = await eventsService.createEvent(req.body, req.user.id);
+
+        return res.status(201).json({ status: "success", payload: newEvent });
     } catch (error) {
         next(error);
     }
--- src/routes/events.router.js
+++ src/routes/events.router.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getEvents } from "../controllers/events.controller.js";
+import { getEvents, createEvent } from "../controllers/events.controller.js";
+import { auth } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/authorize.middleware.js";
 
 const router = Router();
 
 router.get("/", getEvents);
+
+router.post("/", auth, authorizeRoles("organizer", "admin"), createEvent);
 
 export default router;
